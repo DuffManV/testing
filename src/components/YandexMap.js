50eeefcae98988ec9cc2da2dca6c9da87b92779a
DuffManV <wowaccduff@gmail.com>
2024-08-04 18:38:12 +0300

    init

--- src/components/YandexMap.js
+++ src/components/YandexMap.js
@@ -8,9 +8,6 @@ function YandexMap(data) {
                 apikey: 'c80f02e2-d4cd-4bae-9322-4e85e302ef0f',
             }}>
                 <div>
-                    {/*<Map state={{center: [data?.props?.coord?.lat, data?.props?.coord?.long], zoom: 12}}>*/}
-                    {/*    /!*<Placemark geometry={[data?.props?.coord?.lat, data?.props?.coord?.long]}/>*!/*/}
-                    {/*</Map>*/}
                     <Map state={{center: [data?.props?.coord?.lat, data?.props?.coord?.long], zoom: 12}}>
                         <Placemark geometry={[data?.props?.coord?.lat, data?.props?.coord?.long]}/>
                     </Map>
